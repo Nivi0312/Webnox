8a85a06e996f95fe4639bbf323fa709e3e9b4e8d
NIVASHINI M <156221867+Nivi0312@users.noreply.github.com>
2025-02-17 12:51:55 +0530

    Update Signup.js
    add css file

--- socialmedia-master/src/pages/Signup.js
+++ socialmedia-master/src/pages/Signup.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Container, Typography, TextField, Button } from '@mui/material';
+import { Container, Typography, TextField, Button, Box } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import api from '../api';
-
+import './Signup.css';
 
 const Signup = () => {
   const [username, setUsername] = useState('');
@@ -21,44 +21,46 @@ const Signup = () => {
   };
 
   return (
-    <Container className="Signup-container" maxWidth="sm">
-      <Typography className="Signup-title" variant="h4" gutterBottom>
-        Sign Up
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Username"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <TextField
-          label="Email"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          label="Password"
-          type="password"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button className="Signup-button" type="submit" variant="contained" color="primary" fullWidth>
+    <Box className="root">
+      <Container className="Signup-container" maxWidth="sm">
+        <Typography className="Signup-title" variant="h4" gutterBottom>
           Sign Up
-        </Button>
-      </form>
-      <Typography className="Signup-footer" variant="body1" style={{ marginTop: '1rem' }}>
-        Already have an account? <Link to="/signin">Sign In</Link>
-      </Typography>
-    </Container>
+        </Typography>
+        <form className="Signup-form" onSubmit={handleSubmit}>
+          <TextField
+            label="Username"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            label="Email"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button className="Signup-button" type="submit" variant="contained" color="primary" fullWidth>
+            Sign Up
+          </Button>
+        </form>
+        <Typography className="Signup-footer" variant="body1">
+          Already have an account? <Link to="/signin" className="Signup-link">Sign In</Link>
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 
