44ff981bf2988e3be916c3266638763a94fa74bb
NIVASHINI M <156221867+Nivi0312@users.noreply.github.com>
2025-02-17 12:50:53 +0530

    Update Signin.js
    add css file

--- socialmedia-master/src/pages/Signin.js
+++ socialmedia-master/src/pages/Signin.js
@@ -1,9 +1,10 @@
 import React, { useContext, useState } from 'react';
-import { Container, Typography, TextField, Button } from '@mui/material';
+import { Container, Typography, TextField, Button, Box } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import api, { setAuthToken } from '../api';
 import { jwtDecode } from 'jwt-decode';
 import { UserContext } from '../App';
+import './Signin.css';
 
 const Signin = () => {
   const { setUser } = useContext(UserContext);
@@ -25,36 +26,38 @@ const Signin = () => {
   };
 
   return (
-    <Container className="Signin-container" maxWidth="sm">
-      <Typography className="Signin-title" variant="h4" gutterBottom>
-        Sign In
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Email"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          label="Password"
-          type="password"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button className="Signin-button" type="submit" variant="contained" color="primary" fullWidth>
+    <Box className="root">
+      <Container className="Signin-container" maxWidth="sm">
+        <Typography className="Signin-title" variant="h4" gutterBottom>
           Sign In
-        </Button>
-      </form>
-      <Typography className="Signin-footer" variant="body1" style={{ marginTop: '1rem' }}>
-        Don't have an account? <Link to="/signup">Sign Up</Link>
-      </Typography>
-    </Container>
+        </Typography>
+        <form className="Signin-form" onSubmit={handleSubmit}>
+          <TextField
+            label="Email"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button className="Signin-button" type="submit" variant="contained" color="primary" fullWidth>
+            Sign In
+          </Button>
+        </form>
+        <Typography className="Signin-footer" variant="body1">
+          Don't have an account? <Link to="/signup" className="Signin-link">Sign Up</Link>
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 
